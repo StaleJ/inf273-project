414515618b57eef5bef84dae83a52e3e7afec1e5
Ståle <stale.jacobsen@student.uib.no>
2022-02-23 13:03:50 +0100

    Create createTable.sql

---
+++ src/main/kotlin/database/createTable.sql
@@ -0,0 +1,68 @@
+
+CREATE TABLE call_7_vehicle_3
+(
+    id          SERIAL PRIMARY KEY,
+    name        text,
+    average     integer,
+    best        integer,
+    improvement integer,
+    time        real,
+    solution    integer ARRAY
+);
+
+
+CREATE TABLE call_18_vehicle_5
+(
+    id          SERIAL PRIMARY KEY,
+    name        text,
+    average     integer,
+    best        integer,
+    improvement integer,
+    time        real,
+    solution    integer ARRAY
+);
+
+CREATE TABLE call_35_vehicle_7
+(
+    id          SERIAL PRIMARY KEY,
+    name        text,
+    average     integer,
+    best        integer,
+    improvement integer,
+    time        real,
+    solution    integer ARRAY
+);
+
+CREATE TABLE call_80_vehicle_20
+(
+    id          SERIAL PRIMARY KEY,
+    name        text,
+    average     integer,
+    best        integer,
+    improvement integer,
+    time        real,
+    solution    integer ARRAY
+);
+
+CREATE TABLE call_130_vehicle_40
+(
+    id          SERIAL PRIMARY KEY,
+    name        text,
+    average     integer,
+    best        integer,
+    improvement integer,
+    time        real,
+    solution    integer ARRAY
+);
+
+CREATE TABLE call_300_vehicle_90
+(
+    id          SERIAL PRIMARY KEY,
+    name        text,
+    average     integer,
+    best        integer,
+    improvement integer,
+    time        real,
+    solution    integer ARRAY
+);
+
