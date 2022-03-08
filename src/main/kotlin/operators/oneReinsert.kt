33cbcf3d52836c41ffcd0d8538a2972fb807974b
Ståle <stale.jacobsen@student.uib.no>
2022-03-08 19:00:55 +0100

    Update oneReinsert.kt

--- src/main/kotlin/operators/oneReinsert.kt
+++ src/main/kotlin/operators/oneReinsert.kt
@@ -9,24 +9,56 @@ import kotlin.random.Random
 
 private val logger = KotlinLogging.logger {}
 
-fun oneReinsert(currentSolution: MutableList<Int>): List<Int> {
+fun oneReinsert(currentSolution: MutableList<Int>, world: World): MutableList<Int> {
+    val vehicles: HashMap<Int, MutableList<Int>> = hashMapOf()
+    val randomCall = world.calls.random().index + 1
+    val randomVehicle = world.vehicles.random().id
+
+    for (i in 0 until 4) {
+        vehicles[i] = mutableListOf()
+    }
+
+    var i = 0
+    for (c in currentSolution) {
+        if (c == 0) {
+            i++
+        } else {
+            vehicles[i]?.add(c)
+        }
+    }
+
+    println("random call: $randomCall")
+    println("random vehicle: $randomVehicle")
+
+    for (v in vehicles.values) {
+        if (v.contains(randomCall)) {
+            v.removeIf { x -> x == randomCall }
+            break
+        }
+    }
+    if (vehicles[randomVehicle] != null && vehicles[randomVehicle]!!.isEmpty()) {
+        vehicles[randomVehicle]?.add(randomCall)
+        vehicles[randomVehicle]?.add(randomCall)
+    } else {
+        var n = vehicles[randomVehicle]?.size?.let { Random.nextInt(0, it) }
+        vehicles[randomVehicle]?.add(n!!, randomCall)
+        n = vehicles[randomVehicle]?.size?.let { Random.nextInt(0, it) }
+        vehicles[randomVehicle]?.add(n!!, randomCall)
+    }
 
 
-    val from = Random.nextInt(0, currentSolution.size)
-    val a = currentSolution[from]
-    var sublistOne = currentSolution.subList(0, from)
-    var sublistTwo = currentSolution.subList(from + 1, currentSolution.size)
-    var tempList = sublistOne + sublistTwo
-    println(tempList)
-    println(a)
-    val to = Random.nextInt(0, tempList.size)
-    sublistOne = tempList.subList(0, to) as MutableList<Int>
-    sublistTwo = tempList.subList(to, tempList.size) as MutableList<Int>
-    sublistOne.add(a)
-    tempList = sublistOne.plus(sublistTwo)
+    val newSolution = mutableListOf<Int>()
 
-    println(tempList)
-    return tempList
+    for (v in vehicles.values) {
+        newSolution += v
+        newSolution.add(0)
+    }
+    newSolution.removeLast()
+    println(newSolution)
+
+    return newSolution
+
+
 }
 
 internal fun canTakeCall(call: Int, world: World): List<Vehicle> {
@@ -43,8 +75,8 @@ fun main() {
     var sol = listOf(0, 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7)
 
 
-
-    sol = oneReinsert(sol as MutableList<Int>)
-
+    for (i in 0 until 10) {
+        sol = oneReinsert(sol as MutableList<Int>, world)
+    }
 
 }
