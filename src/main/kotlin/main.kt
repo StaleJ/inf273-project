1f4b377f14476cd2077cd83fc25cc63944e4e7f6
Ståle <stale.jacobsen@student.uib.no>
2022-03-09 15:14:41 +0100

    Refactoring main

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,12 +1,17 @@
 import algorithms.localSearch
+import algorithms.simulatedAnnealing
 import classes.World
 import operators.oneReinsertAlgorithm
 import utils.calculateCost
 import utils.parseInput
 import kotlin.system.measureTimeMillis
 
-fun main() {
 
+fun runInstance(
+    algorithm: (s: MutableList<Int>, o: (s: MutableList<Int>, w: World) -> MutableList<Int>, w: World) -> MutableList<Int>,
+    operator: (s: MutableList<Int>, w: World) -> MutableList<Int>,
+    name: String
+) {
     val calls = listOf(7, 18, 35, 80, 130, 300)
     val vehicles = listOf(3, 5, 7, 20, 40, 90)
 
@@ -19,50 +24,27 @@ fun main() {
         var average: Long = 0
         val time = measureTimeMillis {
             for (j in 0 until 10) {
-                val oneInsert = localSearch(initialSolution, ::oneReinsertAlgorithm, world)
-                val oneInsertCost = calculateCost(oneInsert, world)
-                average += oneInsertCost
-                if (oneInsertCost < calculateCost(bestSolution, world)) {
-                    bestSolution = oneInsert
+                val incumbent = algorithm(initialSolution, operator, world)
+                val incumbentCost = calculateCost(incumbent, world)
+                average += incumbentCost
+                if (incumbentCost < calculateCost(bestSolution, world)) {
+                    bestSolution = incumbent
                 }
             }
         }
-        val oneInsertBestCost = calculateCost(bestSolution, world)
-        val onInsertImprovement = 100 * (initialCost - oneInsertBestCost) / initialCost
-        println("Local Search 1-insert |    ${average / 10}   |    $oneInsertBestCost    |    ${onInsertImprovement}%    |    ${time / 1000}s\n")
-        println(bestSolution.toString() + "\n")
-
-    }
-
-
-    for ((i, v) in calls.withIndex()) {
-        println("Instance name (e.g. CALL $v AND VEHICLE ${vehicles[i]})")
-        val world = parseInput("src/main/resources/Call_${v}_Vehicle_${vehicles[i]}.txt")
-        val initialCase = createWorstCase(world)
-        val initialCost = calculateCost(initialCase, world)
-        var best = initialCase
-        var average: Long = 0
-        val timeInMillis = measureTimeMillis {
-            for (j in 0 until 10) {
-                val randomCase = random(world)
-                val randomCost = calculateCost(randomCase, world)
-                average += randomCost
-                if (randomCost < calculateCost(best, world)) {
-                    best = randomCase
-                }
-            }
-        }
-        val bestCost = calculateCost(best, world)
+        val bestCost = calculateCost(bestSolution, world)
         val improvement = 100 * (initialCost - bestCost) / initialCost
         println("              | Average objective | Best objective | Improvement (%) | Running time |")
-        println("Random search |    ${average / 10}   |    $bestCost    |    ${improvement}%    |    ${timeInMillis / 1000}s\n")
-        println(best)
+        println("$name |    ${average / 10}   |    $bestCost    |    ${improvement}%    |    ${(time/1000)}ms\n")
+        println(bestSolution)
         println()
-
-
     }
 
+}
 
 
+fun main() {
+    runInstance(::localSearch, ::oneReinsertAlgorithm, "Local Search-1-insert")
+    runInstance(::simulatedAnnealing, ::oneReinsertAlgorithm, "Simulated Annealing-1-insert")
 
 }
