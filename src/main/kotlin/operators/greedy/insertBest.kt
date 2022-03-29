62d0c31257e3dad142daf6bc5a6215af87b74c8c
Ståle <stale.jacobsen@student.uib.no>
2022-03-29 16:08:14 +0200

    Started working on greedy insert best

--- src/main/kotlin/operators/greedy/insertBest.kt
+++ src/main/kotlin/operators/greedy/insertBest.kt
@@ -1,10 +1,33 @@
 package operators.greedy
 
+import classes.Vehicle
 import classes.World
-import operators.Operator
+import operators.canTakeCall
 
-class InsertBest: Operator<Int>() {
-    override fun operator(currentSolution: MutableList<Int>, world: World): MutableList<Int> {
-        TODO("Not yet implemented")
+fun insertBest(currentSolution: MutableList<Int>, world: World): MutableList<Int> {
+    val vehicles: HashMap<Int, MutableList<Int>> = hashMapOf()
+    val randomCall: Int = world.calls.random().index + 1
+    val vehiclesCanTakeCall: List<Vehicle> = canTakeCall(randomCall, world)
+
+    for (i in 0 until world.vehicles.size + 1) {
+        vehicles[i] = mutableListOf()
     }
+
+    var i = 0
+    for (c in currentSolution) {
+        if (c == 0) {
+            i++
+        } else {
+            vehicles[i]?.add(c)
+        }
+    }
+
+
+    for (v in vehicles.values) {
+        if (v.contains(randomCall)) {
+            v.removeIf { x -> x == randomCall }
+            break
+        }
+    }
+
 }
