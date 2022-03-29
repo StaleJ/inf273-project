aeadd6d8db1446c925462a3b90baf9e89d1fd18a
Ståle <stale.jacobsen@student.uib.no>
2022-03-29 15:11:51 +0200

    Create insertBest.kt

---
+++ src/main/kotlin/operators/greedy/insertBest.kt
@@ -0,0 +1,10 @@
+package operators.greedy
+
+import classes.World
+import operators.Operator
+
+class InsertBest: Operator<Int>() {
+    override fun operator(currentSolution: MutableList<Int>, world: World): MutableList<Int> {
+        TODO("Not yet implemented")
+    }
+}
