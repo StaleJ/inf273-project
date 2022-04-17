67b67fef4ab2a73784f230c5e55a8c3628186e37
Ståle <stale.jacobsen@student.uib.no>
2022-04-17 14:52:26 +0200

    Update Result.kt

--- src/main/kotlin/classes/Result.kt
+++ src/main/kotlin/classes/Result.kt
@@ -1,13 +1,17 @@
 package classes
 
-import org.postgresql.jdbc.PgArray
-
 
 data class Result(
     val name: String,
-    val average: Int,
-    val best: Int,
-    val improvement: Int,
-    val time: Double,
-    val solution: PgArray
-)
+    val average: Long,
+    val best: Long,
+    val improvement: Long,
+    val time: Long,
+    val solution: MutableList<Int>
+) {
+    override fun toString(): String {
+        return "$name | average " + " ".repeat(average.toString().length - 7) + "| best " + " ".repeat(best.toString().length - 4) + "| improvement | time \n " + " ".repeat(
+            name.length
+        ) + "| $average | $best |" + " ".repeat(5) + "$improvement% " + " ".repeat(3) + " | $time ms\n $solution"
+    }
+}
