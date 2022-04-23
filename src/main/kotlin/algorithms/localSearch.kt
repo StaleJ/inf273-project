d38280ad1c3e8f7b0081f6e8972e31eb638a6025
Ståle <stale.jacobsen@student.uib.no>
2022-04-23 14:05:01 +0200

    Update localSearch.kt

--- src/main/kotlin/algorithms/localSearch.kt
+++ src/main/kotlin/algorithms/localSearch.kt
@@ -1,22 +1,26 @@
 package algorithms
 
 import classes.World
+import operators.Operator
 import utils.calculateCost
 import utils.feasibilityCheck
 
-
-fun localSearch(
-    initialSolution: MutableList<Int>, operator: (s: MutableList<Int>, w: World) -> MutableList<Int>, world: World
-): MutableList<Int> {
-    var bestSolution = initialSolution
-    for (i in 0 until 10000) {
-        val newSolution = operator(bestSolution, world)
-        if (feasibilityCheck(newSolution, world).isOk() && calculateCost(newSolution, world) < calculateCost(
-                bestSolution, world
-            )
-        ) {
-            bestSolution = newSolution
+class LocalSearch: Algorithm {
+    override fun runWithOneOperator(
+        initialSolution: MutableList<Int>,
+        operator: Operator,
+        world: World
+    ): MutableList<Int> {
+        var bestSolution = initialSolution
+        for (i in 0 until 10000) {
+            val newSolution = operator.run(bestSolution, world)
+            if (feasibilityCheck(newSolution, world).isOk() && calculateCost(newSolution, world) < calculateCost(
+                    bestSolution, world
+                )
+            ) {
+                bestSolution = newSolution
+            }
         }
+        return bestSolution
     }
-    return bestSolution
 }
