76e52a65615c315be2a3d3a673d7d81e418246d8
Ståle <stale.jacobsen@student.uib.no>
2022-04-26 20:22:21 +0200

    Update threeExchange.kt

--- src/main/kotlin/operators/threeExchange.kt
+++ src/main/kotlin/operators/threeExchange.kt
@@ -3,57 +3,58 @@ package operators
 import classes.World
 import kotlin.random.Random
 
-fun threeExchange(currentSolution: MutableList<Int>, world: World): MutableList<Int> {
-    var randomIndexOne = Random.nextInt(currentSolution.size)
-    var randomIndexTwo = Random.nextInt(currentSolution.size)
-    var randomIndexThree = Random.nextInt(currentSolution.size)
-    var randomCallOne = currentSolution[randomIndexOne]
-    var randomCallTwo = currentSolution[randomIndexTwo]
-    var randomCallThree = currentSolution[randomIndexThree]
+class ThreeExchange : Operator {
+    override fun run(solution: MutableList<Int>, world: World): MutableList<Int> {
+        var randomIndexOne = Random.nextInt(solution.size)
+        var randomIndexTwo = Random.nextInt(solution.size)
+        var randomIndexThree = Random.nextInt(solution.size)
+        var randomCallOne = solution[randomIndexOne]
+        var randomCallTwo = solution[randomIndexTwo]
+        var randomCallThree = solution[randomIndexThree]
 
-    while (randomCallOne == randomCallTwo || randomCallOne == randomCallThree || randomCallTwo == randomIndexThree) {
-        randomIndexOne = Random.nextInt(currentSolution.size)
-        randomIndexTwo = Random.nextInt(currentSolution.size)
-        randomIndexThree = Random.nextInt(currentSolution.size)
-        randomCallOne = currentSolution[randomIndexOne]
-        randomCallTwo = currentSolution[randomIndexTwo]
-        randomCallThree = currentSolution[randomIndexThree]
+        while (randomCallOne == randomCallTwo || randomCallOne == randomCallThree || randomCallTwo == randomIndexThree) {
+            randomIndexOne = Random.nextInt(solution.size)
+            randomIndexTwo = Random.nextInt(solution.size)
+            randomIndexThree = Random.nextInt(solution.size)
+            randomCallOne = solution[randomIndexOne]
+            randomCallTwo = solution[randomIndexTwo]
+            randomCallThree = solution[randomIndexThree]
 
+        }
+
+
+
+        when {
+            randomCallOne == 0 && randomCallTwo == 0 && randomCallThree == 0 -> {
+                return OneInsert().run(solution, world)
+            }
+            randomCallOne != 0 && randomCallTwo != 0 && randomCallThree != 0 -> {
+                val nextIndexOfCallOne = when {
+                    solution.indexOf(randomCallOne) != randomIndexOne -> solution.indexOf(randomCallOne)
+                    else -> solution.lastIndexOf(randomCallOne)
+                }
+                val nextIndexOfCallTwo = when {
+                    solution.indexOf(randomCallTwo) != randomIndexTwo -> solution.indexOf(randomCallTwo)
+                    else -> solution.lastIndexOf(randomCallTwo)
+                }
+                val nextIndexOfCallThree = when {
+                    solution.indexOf(randomCallThree) != randomIndexThree -> solution.indexOf(randomCallThree)
+                    else -> solution.lastIndexOf(randomCallThree)
+                }
+                solution[randomIndexOne] = randomCallTwo
+                solution[nextIndexOfCallOne] = randomCallTwo
+                solution[randomIndexTwo] = randomCallThree
+                solution[nextIndexOfCallTwo] = randomCallThree
+                solution[randomIndexThree] = randomCallOne
+                solution[nextIndexOfCallThree] = randomCallOne
+                return solution
+            }
+            else -> {
+                return OneInsert().run(solution, world)
+
+            }
+
+        }
     }
-
-
-
-    when {
-        randomCallOne == 0 && randomCallTwo == 0 && randomCallThree == 0 -> {
-            return OneInsert().run(currentSolution, world)
-        }
-        randomCallOne != 0 && randomCallTwo != 0 && randomCallThree != 0 -> {
-            val nextIndexOfCallOne = when {
-                currentSolution.indexOf(randomCallOne) != randomIndexOne -> currentSolution.indexOf(randomCallOne)
-                else -> currentSolution.lastIndexOf(randomCallOne)
-            }
-            val nextIndexOfCallTwo = when {
-                currentSolution.indexOf(randomCallTwo) != randomIndexTwo -> currentSolution.indexOf(randomCallTwo)
-                else -> currentSolution.lastIndexOf(randomCallTwo)
-            }
-            val nextIndexOfCallThree = when {
-                currentSolution.indexOf(randomCallThree) != randomIndexThree -> currentSolution.indexOf(randomCallThree)
-                else -> currentSolution.lastIndexOf(randomCallThree)
-            }
-            currentSolution[randomIndexOne] = randomCallTwo
-            currentSolution[nextIndexOfCallOne] = randomCallTwo
-            currentSolution[randomIndexTwo] = randomCallThree
-            currentSolution[nextIndexOfCallTwo] = randomCallThree
-            currentSolution[randomIndexThree] = randomCallOne
-            currentSolution[nextIndexOfCallThree] = randomCallOne
-            return currentSolution
-        }
-        else -> {
-            return OneInsert().run(currentSolution, world)
-
-        }
-
-    }
-
 
 }
