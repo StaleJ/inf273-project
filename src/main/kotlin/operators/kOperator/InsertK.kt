73902dd80dc8634dd8926641364f5f6899c6969a
Ståle <stale.jacobsen@student.uib.no>
2022-04-26 20:22:16 +0200

    Update InsertK.kt

--- src/main/kotlin/operators/kOperator/InsertK.kt
+++ src/main/kotlin/operators/kOperator/InsertK.kt
@@ -37,4 +37,38 @@ class InsertK : Operator {
 
         return newSolution
     }
+
+    // Used in VND
+    //noinspection Duplicates
+    fun kInsert(solution: MutableList<Int>, world: World, k: Int): MutableList<Int> {
+        val vehicles: HashMap<Int, MutableList<Int>> = createVehicleMap(solution, world)
+        val callsToTake: MutableList<Int> = mutableListOf()
+        val listCalls: MutableSet<Int> = mutableSetOf()
+        val newSolution: MutableList<Int> = mutableListOf()
+
+
+        for (i in 1..world.calls.size) {
+            callsToTake.add(i)
+        }
+
+        for (i in 1 until k) {
+            val randomCall: Int = callsToTake.random()
+            callsToTake.removeIf { randomCall == it }
+            listCalls.add(randomCall)
+            removeCallFromVehicle(randomCall, vehicles)
+        }
+
+        for (call in listCalls) {
+            val vehicle = canTakeCall(call, world).random().id
+            insertCallInVehicle(call, vehicle, vehicles)
+        }
+
+        for (v in vehicles.values) {
+            newSolution += v
+            newSolution.add(0)
+        }
+        newSolution.removeLast()
+
+        return newSolution
+    }
 }
