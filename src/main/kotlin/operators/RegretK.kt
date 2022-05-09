cfe9713815a25ec92e6c92fce82f018967732a34
Ståle <stale.jacobsen@student.uib.no>
2022-05-09 13:55:20 +0200

    Update RegretK.kt

--- src/main/kotlin/operators/RegretK.kt
+++ src/main/kotlin/operators/RegretK.kt
@@ -1,23 +1,56 @@
 package operators
 
+import classes.Vehicle
 import classes.World
 import utils.calculateCost
+import utils.feasibilityCheck
 import utils.parseInput
+import java.util.Collections.asLifoQueue
 import java.util.Collections.max
+import java.util.Deque
+import java.util.Queue
 
 class RegretK : Operator {
     override fun run(solution: MutableList<Int>, world: World): MutableList<Int> {
-        return solution
+        return runRegretK(solution, runRandomRemove(solution, world, 3), world, 3)
     }
 
-    fun runGreedyRemove(solution: MutableList<Int>, world: World, k: Int): MutableList<Int> {
+    private fun runRandomRemove(solution: MutableList<Int>, world: World, k: Int): MutableList<Int>{
+        val vehicles: HashMap<Int, MutableList<Int>> = createVehicleMap(solution, world)
+        val callsToTake: MutableList<Int> = mutableListOf()
+        val listCalls: MutableSet<Int> = mutableSetOf()
+
+
+        for (i in 1..world.calls.size) {
+            callsToTake.add(i)
+        }
+
+        for (i in 1 until k) {
+            val randomCall: Int = callsToTake.random()
+            callsToTake.removeIf { randomCall == it }
+            listCalls.add(randomCall)
+            removeCallFromVehicle(randomCall, vehicles)
+        }
+        return listCalls.toMutableList()
+    }
+
+
+    /**
+     * Run greedy remove k element.
+     *
+     * @param solution solution we want to run greedy remove on.
+     * @param world world that the solution lives in.
+     * @param k number of calls to be greedy removed.
+     * @return calls that is removed.
+     */
+    private fun runGreedyRemove(solution: MutableList<Int>, world: World, k: Int): MutableList<Int> {
+        solution.map { it }
         val callsToInsert: MutableList<Int> = mutableListOf()
         val before: Long = calculateCost(solution, world)
         for (i in 0 until k) {
             val tempMap: HashMap<Long, Int> = hashMapOf()
-
-            for (j in 0 until solution.size) {
-                val call = solution[j]
+            val copySolution = solution.map { it }
+            for (call in copySolution) {
                 if (call != 0) {
                     // Remove and calculate cost
                     val pickupCall = solution.indexOf(call)
@@ -34,60 +67,152 @@ class RegretK : Operator {
             }
             val maxDeltaCost: Long = max(tempMap.keys)!!
             val maxCall: Int = tempMap[maxDeltaCost]!!
-            solution.removeAt(maxCall)
-            solution.removeAt(maxCall)
-            println(max(tempMap.keys))
+            solution.remove(maxCall)
+            solution.remove(maxCall)
             callsToInsert.add(tempMap[max(tempMap.keys)]!!)
+        }
+        for (call in callsToInsert) {
+            solution.add(call)
+            solution.add(call)
+        }
+
+
+        return callsToInsert
+    }
+
+    private fun insertBest(solution: MutableList<Int>, world: World, call: Int): MutableList<Int> {
+        val vehicles: HashMap<Int, MutableList<Int>> = createVehicleMap(solution, world)
+        val randomCall: Int = call
+        val vehiclesCanTakeCall: List<Vehicle> = canTakeCall(randomCall, world)
+        val idOfVehiclesCanTakeCall: MutableList<Int> = mutableListOf()
+        vehiclesCanTakeCall.forEach { v -> idOfVehiclesCanTakeCall.add(v.id) }
+
+
+
+        for (v in vehicles.values) {
+            if (v.contains(randomCall)) {
+                v.removeIf { x -> x == randomCall }
+                break
+            }
+        }
+
+        // Start trying to find best place to put call
+        var bestSolution = solution
+        for ((id, car) in vehicles) {  // Loop through all cars
+            if (id in idOfVehiclesCanTakeCall) {
+                if (car.isEmpty()) { // there is only one place in the vehicle to put call
+                    car.add(randomCall)
+                    car.add(randomCall)
+                    val tempSolution = createSolution(vehicles)
+                    if (feasibilityCheck(tempSolution, world).isOk() && calculateCost(tempSolution,
+                            world) < calculateCost(
+                            bestSolution,
+                            world
+                        )
+                    ) {
+                        bestSolution = tempSolution
+                    }
+                    car.remove(randomCall)
+                    car.remove(randomCall)
+                } else {
+
+                    for (k in 0 until car.size - 1) {
+                        for (l in k + 1 until car.size) {
+                            car.add(k, randomCall)
+                            car.add(l, randomCall)
+                            val tempSolution = createSolution(vehicles)
+                            if (feasibilityCheck(tempSolution, world).isOk() && calculateCost(
+                                    tempSolution,
+                                    world
+                                ) < calculateCost(bestSolution, world)
+                            ) {
+                                bestSolution = tempSolution
+                            }
+                            car.remove(randomCall)
+                            car.remove(randomCall)
+                        }
+                    }
+
+                }
+
+            }
+        }
+        return bestSolution
+    }
+
+
+    fun runRegretK(solution: MutableList<Int>, insertCalls: MutableList<Int>, world: World, k: Int): MutableList<Int> {
+
+        val regretValueList: HashMap<Int, Long> = hashMapOf()
+        // Loop over calls we want to insert
+        for (call in insertCalls) {
+            regretValueList[call] = 0
+            val vehicles: HashMap<Int, MutableList<Int>> = createVehicleMap(solution, world)
+            for (v in vehicles.values) {
+                if (v.contains(call)) {
+                    v.removeIf { it == call }
+                    break
+                }
+            }
+            val regretQueue: ArrayDeque<Long> = ArrayDeque()
+            // Test alle possible position in the solution
+            for ((id, car) in vehicles) {
+                if (car.isEmpty()) { // there is only one place in the vehicle to put call
+                    car.add(call)
+                    car.add(call)
+                    val tempSolution = createSolution(vehicles)
+                    if (feasibilityCheck(tempSolution, world).isOk()) {
+                        val cost: Long = calculateCost(tempSolution, world)
+                        if (!regretQueue.contains(cost))
+                            regretQueue.add(calculateCost(tempSolution, world))
+                    }
+
+
+                    car.remove(call)
+                    car.remove(call)
+                } else {
+                    for (i in 0 until car.size - 1) {
+                        for (j in i + 1 until car.size) {
+                            car.add(i, call)
+                            car.add(j, call)
+                            val tempSolution = createSolution(vehicles)
+
+                            if (feasibilityCheck(tempSolution, world).isOk()) {
+                                val cost: Long = calculateCost(tempSolution, world)
+                                if (!regretQueue.contains(cost))
+                                    regretQueue.add(calculateCost(tempSolution, world))
+                            }
+
+
+                            car.remove(call)
+                            car.remove(call)
+                        }
+                    }
+                }
+            }
+            if (regretQueue.isEmpty())
+                return solution
+            regretQueue.sort()
+            while (regretQueue.isNotEmpty()) {
+                    regretValueList[call] = regretValueList[call]!! + regretQueue.removeFirst()
+            }
+
 
         }
-        println(callsToInsert)
-        return solution
+        val regretCall = max(regretValueList.entries, java.util.Map.Entry.comparingByValue()).key
+
+
+        return insertBest(solution, world, regretCall)
+
     }
+
 
 }
 
+
 fun main() {
-    val world = parseInput("src/main/resources/Call_18_Vehicle_5.txt")
-    val list = listOf(4,
-        4,
-        15,
-        15,
-        14,
-        14,
-        16,
-        16,
-        0,
-        6,
-        6,
-        5,
-        5,
-        17,
-        17,
-        13,
-        13,
-        0,
-        8,
-        18,
-        8,
-        18,
-        0,
-        9,
-        9,
-        12,
-        12,
-        0,
-        7,
-        7,
-        3,
-        3,
-        10,
-        1,
-        10,
-        1,
-        0,
-        2,
-        11,
-        2,
-        11).toMutableList()
-    println(RegretK().runGreedyRemove(list, world, 4))
+    val world = parseInput("src/main/resources/Call_7_Vehicle_3.txt")
+    val list = mutableListOf(0, 0, 1, 1, 0, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7)
+    val regret = RegretK()
+    println(regret.run(list, world))
 }
